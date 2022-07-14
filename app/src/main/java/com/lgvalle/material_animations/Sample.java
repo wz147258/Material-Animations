3f6f0bb0ecd68a375d94b5b7703025bc443b79c8
wangzheng <wangzheng.20210419@bytedance.com>
2022-07-14 21:01:35 +0800

    更新gradle

--- app/src/main/java/com/lgvalle/material_animations/Sample.java
+++ app/src/main/java/com/lgvalle/material_animations/Sample.java
@@ -1,9 +1,9 @@
 package com.lgvalle.material_animations;
 
-import androidx.databinding.BindingAdapter;
-import androidx.annotation.ColorRes;
-import androidx.core.graphics.drawable.DrawableCompat;
 import android.widget.ImageView;
+import androidx.annotation.ColorInt;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.databinding.BindingAdapter;
 
 import java.io.Serializable;
 
@@ -15,13 +15,13 @@ public class Sample implements Serializable {
     final int color;
     private final String name;
 
-    public Sample(@ColorRes int color, String name) {
+    public Sample(@ColorInt int color, String name) {
         this.color = color;
         this.name = name;
     }
 
     @BindingAdapter("bind:colorTint")
-    public static void setColorTint(ImageView view, @ColorRes int color) {
+    public static void setColorTint(ImageView view, @ColorInt int color) {
         DrawableCompat.setTint(view.getDrawable(), color);
         //view.setColorFilter(color, PorterDuff.Mode.SRC_IN);
     }
